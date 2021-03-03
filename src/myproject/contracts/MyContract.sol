e24d93e38dcad54e11935849742658d143fee4db
Manwel Bugeja <manwel.bugeja.18@um.edu.mt>
2021-03-03 13:44:39 +0100

    Fixed tabbing

--- src/myproject/contracts/MyContract.sol
+++ src/myproject/contracts/MyContract.sol
@@ -1,3 +1,4 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.6;
 
@@ -5,128 +6,132 @@ pragma solidity ^0.6.6;
 contract MyContract {
     
 
-  function test() public pure returns(bool) {
-    return true;
-  }
-
-  struct LoanRequest{
-    uint    creationDate;
-    address payable borrower;
-    uint    amount;
-    uint    expiryDate;
-    uint    interestPaid;
-
-    address payable guarantor;
-    uint    guarantorInterest;
-    bool    guaranteeAccapted;
-
-    address payable loaner;
-  }
+    function test() public pure returns(bool) {
+        return true;
+    }
   
-  LoanRequest[] loanRequests;
+    struct LoanRequest{
+        uint    creationDate;
+        address payable borrower;
+        uint    amount;
+        uint    expiryDate;
+        uint    interestPaid;
   
-  function getLoanRequestCount() public view returns(uint) {
-    return loanRequests.length;  
-  }
-
-  function getLoanRequest(uint index) public view returns(
-    address, 
-    uint, 
-    uint,
-    uint, 
-    address,
-    uint,
-    address) {
-
-    return (
-      loanRequests[index].borrower,
-      loanRequests[index].amount,
-      loanRequests[index].expiryDate,
-      loanRequests[index].interestPaid,
-      loanRequests[index].guarantor,
-      loanRequests[index].guarantorInterest,
-      loanRequests[index].loaner
-    );
-  }
-
-
-  // Borrower Functions
+        address payable guarantor;
+        uint    guarantorInterest;
+        bool    guaranteeAccapted;
+  
+        address payable loaner;
+    }
     
-  function submitLoanRequest(uint amount, uint expiryDate, uint interestPaid) public returns(uint) {
-
-    loanRequests.push(LoanRequest(
-      now,
-      msg.sender,
-      amount, 
-      expiryDate, 
-      interestPaid,
-      address(0),
-      0,
-      false,
-      address(0)
-    ));
-
-    return loanRequests.length;
-  }
+    LoanRequest[] loanRequests;
+    
+    function getLoanRequestCount() public view returns(uint) {
+        return loanRequests.length;  
+    }
   
-  function acceptGuarantee(uint index) public {
+    function getLoanRequest(uint index) public view returns(
+        address, 
+        uint, 
+        uint,
+        uint, 
+        address,
+        uint,
+        address) {
+  
+        return (
+            loanRequests[index].borrower,
+            loanRequests[index].amount,
+            loanRequests[index].expiryDate,
+            loanRequests[index].interestPaid,
+            loanRequests[index].guarantor,
+            loanRequests[index].guarantorInterest,
+            loanRequests[index].loaner
+        );
+    }
+    
+    function hasLoanExpired(uint index) public view returns(bool) {
       
-    // Check that the caller is the person who submitted the request
-    require(msg.sender == loanRequests[index].borrower, "Dude, you are not the person who submitted the request!");
-    
-    // Transfer money from contract to Borrower
-    loanRequests[index].borrower.transfer(loanRequests[index].amount);
-    
-    // Mark the guarantee as guarantee as accapted
-    loanRequests[index].guaranteeAccapted = true;
-  }
-  
-  function declineGuarantee(uint index) public {
-    // Check that the caller is the person who submitted the request
-    require(msg.sender == loanRequests[index].borrower, "Dude, you are not the person who submitted the request!");
-    
-    // Transfer money from contract to Guarantor
-    loanRequests[index].guarantor.transfer(loanRequests[index].amount);
-    
-    // Remove guarantee from request
-    loanRequests[index].guarantor = address(0);
-    loanRequests[index].guarantorInterest = 0;
-  }
-
-
-  // Guarantor Functions
-    
-  function guaranteeLoan(uint index, uint interest) public payable returns(uint){
-    // Check that the loan is not guaranteed
-    require(loanRequests[index].guarantor == address(0), "Loan already guaranteed");
-    // Check that the guarantor has enough balance
-    require(msg.value >= loanRequests[index].amount, "Not enough money brotha");
-    
-    // Store guarantee in contract done by default
-
-    // Update the loan request information
-    loanRequests[index].guarantor = msg.sender;
-    loanRequests[index].guarantorInterest = interest;
-  }
+    }
   
   
-  // Loaner Functions
-  
-  function provideLoan(uint index) public payable{
+    // Borrower Functions
       
-    // We need to check that the loan request is guaranteed
-    require(loanRequests[index].guarantor != address(0), "Loan not guaranteed !");
+    function submitLoanRequest(uint amount, uint expiryDate, uint interestPaid) public returns(uint) {
+  
+        loanRequests.push(LoanRequest(
+            now,
+            msg.sender,
+            amount, 
+            expiryDate, 
+            interestPaid,
+            address(0),
+            0,
+            false,
+            address(0)
+        ));
+  
+        return loanRequests.length;
+    }
     
-    // We need to check that the loaner has enough balance
-    require(msg.value >= loanRequests[index].amount, "Insufficient balance brotha");
+    function acceptGuarantee(uint index) public {
+        
+        // Check that the caller is the person who submitted the request
+        require(msg.sender == loanRequests[index].borrower, "Dude, you are not the person who submitted the request!");
+        
+        // Transfer money from contract to Borrower
+        loanRequests[index].borrower.transfer(loanRequests[index].amount);
+        
+        // Mark the guarantee as guarantee as accapted
+        loanRequests[index].guaranteeAccapted = true;
+    }
+    
+    function declineGuarantee(uint index) public {
+        // Check that the caller is the person who submitted the request
+        require(msg.sender == loanRequests[index].borrower, "Dude, you are not the person who submitted the request!");
+        
+        // Transfer money from contract to Guarantor
+        loanRequests[index].guarantor.transfer(loanRequests[index].amount);
+        
+        // Remove guarantee from request
+        loanRequests[index].guarantor = address(0);
+        loanRequests[index].guarantorInterest = 0;
+    }
+  
+  
+    // Guarantor Functions
+      
+    function guaranteeLoan(uint index, uint interest) public payable returns(uint){
+        // Check that the loan is not guaranteed
+        require(loanRequests[index].guarantor == address(0), "Loan already guaranteed");
+        // Check that the guarantor has enough balance
+        require(msg.value >= loanRequests[index].amount, "Not enough money brotha");
+        
+        // Store guarantee in contract done by default
+  
+        // Update the loan request information
+        loanRequests[index].guarantor = msg.sender;
+        loanRequests[index].guarantorInterest = interest;
+    }
     
     
-    // Transfer Money
-    loanRequests[index].borrower.transfer(loanRequests[index].amount);
+    // Loaner Functions
     
-    // Update loan request
-    loanRequests[index].loaner = msg.sender;
-  }
+    function provideLoan(uint index) public payable{
+          
+        // We need to check that the loan request is guaranteed
+        require(loanRequests[index].guarantor != address(0), "Loan not guaranteed !");
+        
+        // We need to check that the loaner has enough balance
+        require(msg.value >= loanRequests[index].amount, "Insufficient balance brotha");
+        
+        
+        // Transfer Money
+        loanRequests[index].borrower.transfer(loanRequests[index].amount);
+        
+        // Update loan request
+        loanRequests[index].loaner = msg.sender;
+    }
 }
 
 
